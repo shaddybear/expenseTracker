dda76d3af8300fb4596b800cca7846a31cb35acb
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-04-07 17:45:04 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router'
 
 
 const index = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/welcome')
+        }, 2000);
+    },[])
   return (
     <View style={styles.container}>
       <Image
