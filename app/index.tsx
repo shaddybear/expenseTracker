c8762408cb1732a74d9d1d841885d9c4b79bef09
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-04-07 17:40:18 +0530

    Splash Screen logo

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { colors } from '@/constants/theme'
+
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        source={require("../assets/images/splashImage.png")}
+        />
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: colors.neutral900,
+    },
+    logo: {
+        height: '20%',
+        aspectRatio: 1,
+    }
+})
