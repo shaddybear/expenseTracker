397849d64006bcc5cb7b26f75acde73acb9c6b9d
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-04-07 21:42:18 +0530

    Login screen somewhat done

---
+++ components/Button.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { CustomButtonProps } from '@/types'
+import { colors, radius } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
+
+const Button = ({
+    style,
+    onPress,
+    loading = false,
+    children
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
+        {children}
+    </TouchableOpacity>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: colors.primary,
+        borderRadius: radius._17,
+        borderCurve: "continuous",
+        height: verticalScale(52),
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
