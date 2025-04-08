56b21da718cd9becc65efdea0a0a0355155676ad
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-04-08 09:27:17 +0530

    Update welcome.tsx

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -6,14 +6,16 @@ import { colors, spacingX, spacingY } from '@/constants/theme'
 import { verticalScale } from '@/utils/styling'
 import Button from '@/components/Button'
 import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+    const router = useRouter();
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         {/* Login button & Image */}
         <View>
-            <TouchableOpacity style={styles.loginButton}>
+            <TouchableOpacity onPress={() => router.push('/(auth)/login')} style={styles.loginButton}>
                 <Typo fontWeight={"500"}>Sign in</Typo>
             </TouchableOpacity>
 
@@ -46,7 +48,7 @@ const Welcome = () => {
             </Animated.View>
 
             <Animated.View entering={FadeInDown.duration(1000).delay(200).springify().damping(12)} style={styles.buttonContainer}>
-            <Button>
+            <Button onPress={() => router.push('/(auth)/register')}>
                 <Typo size={22} color={colors.neutral900} fontWeight={"600"}>
                     Get Started
                 </Typo>
