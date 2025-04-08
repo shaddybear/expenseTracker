2fe35ae0b5a88872e0d1610dd3a76a05ac6d8e6e
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-04-08 14:59:03 +0530

    New user register

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -9,6 +9,7 @@ import Input from '@/components/Input'
 import * as Icons from 'phosphor-react-native'
 import Button from '@/components/Button'
 import { useRouter } from 'expo-router'
+import { useAuth } from '@/contexts/authContext'
 
 const Register = () => {
      
@@ -17,18 +18,19 @@ const Register = () => {
     const nameRef = useRef("");
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
+    const {register: registerUser} = useAuth();
     const handleSubmit = async ()=>{
         if(!emailRef.current || !passwordRef.current || !nameRef.current){
             Alert.alert('Sign up', "Please fill all the fields");
             return;
         }
-        {/*
-            Make sure to remove the 4 console logs
-        */}
-        console.log("email: ", emailRef.current)
-        console.log("password: ", passwordRef.current)
-        console.log("name: ", nameRef.current)
-        console.log("Good to go")
+        setIsLoading(true)
+        const res = await registerUser(emailRef.current, passwordRef.current, nameRef.current);
+        setIsLoading(false);
+        console.log("register result: ", res)
+        if(!res.success){
+            Alert.alert("Sign up", res.msg)
+        }
     }
 
   return (
